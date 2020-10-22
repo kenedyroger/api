6baf6fc1e564e928b3b67accfe31099afbd5e3ad
Kenedy Roger <kenedyrms@gmail.com>
2020-10-21 21:58:53 -0300

    It ensure system don't signup an user with missing username parameter

--- src/useCases/userSignUp.spec.ts
+++ src/useCases/userSignUp.spec.ts
@@ -1,7 +1,9 @@
 class User {
-    private readonly email: string;
+    private readonly username: string;
+    private readonly email?: string;
 
-    constructor(userData: { email: string }) {
+    constructor(userData: { email: string, username: string }) {
+        this.username = userData.username;
         this.email = userData.email;
     }
 }
@@ -15,7 +17,10 @@ class UserAccountRepositorySpy implements IUserAccountRepository {
 
     getUserByEmail(email: string): Promise<User> {
         if (this.email === email) {
-            const user = new User({email});
+            const user = new User({
+                username: "username",
+                email
+            });
             return Promise.resolve(user);
         }
         return Promise.resolve(undefined);
@@ -27,18 +32,38 @@ class SingUpUser {
     constructor(private readonly userAccountRepository: IUserAccountRepository) {
     }
 
-    async signUp(userData: { email: string }) {
-        const existingUser = await this.userAccountRepository.getUserByEmail(userData.email);
-        if (existingUser)
+    async signUp(userData: { email: string, username: string }) {
+        if (!userData.username || !userData.email.length)
+            throw new Error("Parâmetro faltando username");
+        if (await this.emailAlreadyInUse(userData.email))
             throw new Error("Usuário já existe");
+    }
+
+    private async emailAlreadyInUse(email: string): Promise<boolean> {
+        const existingUser = this.userAccountRepository.getUserByEmail(email);
+        return !!existingUser;
     }
 }
 
 describe('User SingUp', () => {
+
     it('should not singUp user if email already exists', async function () {
-        const userData = {email: "some_valid_email"};
+        const userData = {
+            username: "valid_user_name",
+            email: "some_valid_email"
+        };
         const userAccountRepository = new UserAccountRepositorySpy();
         const sut = new SingUpUser(userAccountRepository);
         await expect(sut.signUp(userData)).rejects.toThrow("Usuário já existe");
+    });
+
+    it('should not signUp an user if username is not provided', async function () {
+        const userData = {
+            username: null,
+            email: "some_new_valid_email"
+        };
+        const userAccountRepository = new UserAccountRepositorySpy();
+        const sut = new SingUpUser(userAccountRepository);
+        await expect(sut.signUp(userData)).rejects.toThrow("Parâmetro faltando username");
     });
 })
